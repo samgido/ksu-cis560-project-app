c16cde648e9dfc5b45ef48563489d973429ce86f
Samuel Jackson Gido <132026917+samgido@users.noreply.github.com>
2025-10-31 23:15:51 -0500

    add base code

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
+# KSU CIS560 project app
+
+## Setup
+Create a .env file in the base directory definining some variables to establish a connection with the database. Contents should look something like this
+```
+SQL_CONNECTION_STRING="Driver={ODBC Driver 18 for SQL Server};Server=<server_name>;Database={<database_name>};Encrypt=yes;TrustServerCertificate=no;Authentication=ActiveDirectoryInteractive"
+```
+More info [here](https://learn.microsoft.com/en-us/sql/connect/python/pyodbc/python-sql-driver-pyodbc-quickstart?view=sql-server-ver17&tabs=sql-server)
+
+## Usage
 To run project to http://localhost:5000
 ```
 uv run ./main.py
--- main.py
+++ main.py
@@ -1,4 +1,41 @@
+from os import getenv
 from flask import Flask
+from dotenv import load_dotenv
+import pyodbc 
+
+if not load_dotenv():
+    print("Environment file not found, see readme")
+    exit(1)
+
+def get_env_or_exit(env_name: str):
+    val = getenv(env_name)
+
+    if (val is None):
+        print("Could not get env " + env_name)
+        exit(1)
+
+    return val
+
+connection_string = get_env_or_exit("SQL_CONNECTION_STRING")
+
+connection = pyodbc.connect(connection_string)
+
+print(connection.getinfo(pyodbc.SQL_DATABASE_NAME))
+
+cursor = connection.cursor()
+
+cursor.execute("""
+    select count(*) from test
+""")
+
+cursor.commit()
+
+rows = cursor.fetchall()
+
+for row in rows:
+    print(row)
+
+exit(0)
 
 app = Flask(__name__)
 
