67afb8b29f5e4ecc2619a930814c5540c602f45a
Samuel Jackson Gido <132026917+samgido@users.noreply.github.com>
2025-11-02 00:06:38 -0500

    cleanup code, add base template

--- main.py
+++ main.py
@@ -1,50 +1,26 @@
-from os import getenv
-from flask import Flask
-from dotenv import load_dotenv
+import os 
+import flask 
+import dotenv 
 import pyodbc 
+import util
 
-if not load_dotenv():
+if not dotenv.load_dotenv():
     print("Environment file not found, see readme")
     exit(1)
 
-def get_env_or_exit(env_name: str):
-    val = getenv(env_name)
-
-    if (val is None):
-        print("Could not get env " + env_name)
-        exit(1)
-
-    return val
-
-connection_string = get_env_or_exit("SQL_CONNECTION_STRING")
-
-connection = pyodbc.connect(connection_string)
-
-print(connection.getinfo(pyodbc.SQL_DATABASE_NAME))
-
-cursor = connection.cursor()
-
-cursor.execute("""
-    select count(*) from test
-""")
-
-cursor.commit()
-
-rows = cursor.fetchall()
-
-for row in rows:
-    print(row)
-
-exit(0)
-
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 @app.route("/")
 def index():
-    return """
-    <p>
-        books
-    </p>
-    """
+    s = "<p>"
 
-app.run(debug=True)
+    for row in manager.get_rows("select * from test"):
+        s += str(row) + "\t"
+
+    return s + "</p>"
+
+if __name__ == "__main___":
+    global manager
+    manager = util.ConnectionManager()
+
+    app.run(debug=True)
