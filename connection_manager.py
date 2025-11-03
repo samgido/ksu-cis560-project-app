650d60fdf567e426a48f733b5ba484049594df1d
Samuel Jackson Gido <132026917+samgido@users.noreply.github.com>
2025-11-03 08:44:41 -0600

    address pr comment

--- connection_manager.py
+++ connection_manager.py
@@ -5,17 +5,22 @@ class ConnectionManager:
 		check_dotenv()
 
 		self.connection = create_database_connection()
-		self.cursor = self.connection.cursor()
 
 	def get_rows(self, query: str):
-		self.cursor.execute(query)
-		return self.cursor.fetchall()
+		cursor = self.connection.cursor()
+		cursor.execute(query)
+		rows = cursor.fetchall()
+
+		cursor.close()
+		return rows
 
 	def dispose(self):
 		self.connection.close()
 
 	def email_belongs_to_customer(self, email):
+		print("Warning: Email belongs to customer not implemented")
 		return email == "johndoe@gmail.com" # placeholder
 
 	def book_available_for_checkout(self, book_id):
+		print("Warning: Book available for checkout not implemented")
 		return book_id == 1 # placeholder
