6ca847f953bf0d234e199ad7beec5ec6861523ad
Samuel Jackson Gido <132026917+samgido@users.noreply.github.com>
2025-11-30 22:15:34 -0600

    add view class, shrink main.py

--- service.py
+++ service.py
@@ -6,247 +6,242 @@ from utils import check_dotenv
 from dataclasses import dataclass
 import utils
 
+
 @dataclass
 class ListDisplayBook:
-	book_id: int
-	cover_img_url: str
-	title: str
-	genre: str
-	available: bool
-	available_count: int
+    book_id: int
+    cover_img_url: str
+    title: str
+    genre: str
+    available: bool
+    available_count: int
+
 
 @dataclass
 class Book:
-	book_id: int
-	isbn: int
-	cover_img_url: str
-	author: str
-	title: str
-	genre: str
-	available: bool
-	available_count: int
-	total_count: int
+    book_id: int
+    isbn: int
+    cover_img_url: str
+    author: str
+    title: str
+    genre: str
+    available: bool
+    available_count: int
+    total_count: int
+
 
 @dataclass
 class User:
-	customer_id: int
-	email: str
-	name: str
-	card_inactive: bool
+    customer_id: int
+    email: str
+    name: str
+    card_inactive: bool
+
 
 @dataclass
 class Checkout:
-	checkout_date: date
-	due_date: date
-	book_id: int
-	condition: str
-	overdue: bool
-	checkout_id: int
-	book_copy_id: int
+    checkout_date: date
+    due_date: date
+    book_id: int
+    condition: str
+    overdue: bool
+    checkout_id: int
+    book_copy_id: int
+
 
 class Service:
-	def __init__(self, repository: Repository) -> None:
-		check_dotenv()
-		self.repo = repository
-		
-		self.get_book_count()
+    def __init__(self, repository: Repository) -> None:
+        check_dotenv()
+        self.repo = repository
 
-		self.conditions = []
-		self.initialize_conditions()
+        self.get_book_count()
 
-	def get_analytics_data(self, query_name, begin_date, end_date):
-		rows = self.repo.run_analytics(query_name, begin_date, end_date)
-		return rows
+        self.conditions = []
+        self.initialize_conditions()
 
-	def initialize_conditions(self):
-		rows = self.repo.get_condition_names()
+    def get_analytics_data(self, query_name, begin_date, end_date):
+        rows = self.repo.run_analytics(query_name, begin_date, end_date)
+        return rows
 
-		try:
-			self.conditions = list(map(lambda r: r.condition, rows))
-		except:
-			print("Could not initialize condition names")
-			exit(1)
+    def initialize_conditions(self):
+        rows = self.repo.get_condition_names()
 
-	def get_checkout(self, email, book_id) -> Optional[Checkout]:
-		rows = self.repo.get_checkouts(email, book_id)
+        try:
+            self.conditions = list(map(lambda r: r.condition, rows))
+        except:
+            print("Could not initialize condition names")
+            exit(1)
 
-		if len(rows) == 0:
-			return None
+    def get_checkout(self, email, book_id) -> Optional[Checkout]:
+        rows = self.repo.get_checkouts(email, book_id)
 
-		return self.make_checkout(rows[0])
+        if len(rows) == 0:
+            return None
 
-	def return_book(self, checkout_id, book_copy_id, condition) -> Optional[str]:
-		if self.repo.return_book(checkout_id) == 0:
-			return "Failed to return book"
+        return self.make_checkout(rows[0])
 
-		if self.repo.update_condition(book_copy_id, condition) == 0:
-			return "Failed to update book condition"
+    def return_book(self, checkout_id, book_copy_id, condition) -> Optional[str]:
+        if self.repo.return_book(checkout_id) == 0:
+            return "Failed to return book"
 
-		return None
+        if self.repo.update_condition(book_copy_id, condition) == 0:
+            return "Failed to update book condition"
 
-	def checkout_book(self, book_id, loaner_email) -> Optional[str]:
-		if not self.book_available_for_checkout(book_id):
-			return f"Book {book_id} unavailable for checkout"
+        return None
 
-		if not self.repo.get_num_accounts(loaner_email):
-			return f"Email {loaner_email} doesn't belong to a customer"
+    def checkout_book(self, book_id, loaner_email) -> Optional[str]:
+        if not self.book_available_for_checkout(book_id):
+            return f"Book {book_id} unavailable for checkout"
 
-		if self.repo.create_checkout(loaner_email, book_id) == 0:
-			return "Failed to create checkout"
+        if not self.repo.get_num_accounts(loaner_email):
+            return f"Email {loaner_email} doesn't belong to a customer"
 
-		return None
+        if self.repo.create_checkout(loaner_email, book_id) == 0:
+            return "Failed to create checkout"
 
-	def create_customer(self, email, first_name, last_name) -> Optional[str]:
-		error = ""
+        return None
 
-		rows = self.repo.get_user(email)
-		if len(rows) > 0:
-			error += f"Email {email} already belongs to a customer, their card has been activated.\n"
+    def create_customer(self, email, first_name, last_name) -> Optional[str]:
+        error = ""
 
-		self.repo.create_customer(email, first_name, last_name)
+        rows = self.repo.get_user(email)
+        if len(rows) > 0:
+            error += f"Email {email} already belongs to a customer, their card has been activated.\n"
 
-		return error
+        self.repo.create_customer(email, first_name, last_name)
 
-	def disable_library_card(self, email) -> Optional[str]:
-		rows = self.repo.get_user(email)
+        return error
 
-		if len(rows) == 0:
-			return "Error: Email doesn't belong to a customer"
+    def disable_library_card(self, email) -> Optional[str]:
+        rows = self.repo.get_user(email)
 
-		user = self.make_user(rows[0])
+        if len(rows) == 0:
+            return "Error: Email doesn't belong to a customer"
 
-		if user is None:
-			return "Error occurred reading user"
+        user = self.make_user(rows[0])
 
-		if user.card_inactive:
-			return "Card is already inactive"
+        if user is None:
+            return "Error occurred reading user"
 
-		if len(self.repo.get_users_checked_books(email)) > 0:
-			return "Error: User has checked out books"
+        if user.card_inactive:
+            return "Card is already inactive"
 
-		rows_affected = self.repo.disable_library_card(email)
+        if len(self.repo.get_users_checked_books(email)) > 0:
+            return "Error: User has checked out books"
 
-		if rows_affected == 0:
-			return "Error occurred disabling card"
+        rows_affected = self.repo.disable_library_card(email)
 
-		return None
+        if rows_affected == 0:
+            return "Error occurred disabling card"
 
-	def get_book_count(self) -> int:
-		return self.repo.get_book_count()
+        return None
 
-	def get_books_list_display(self, page_number) -> Optional[List[ListDisplayBook]]:
-		rows = self.repo.get_book_list_display(page_number)
-		books = list(map(self.make_display_book, rows))
+    def get_book_count(self) -> int:
+        return self.repo.get_book_count()
 
-		return utils.all_or_none(books)
+    def get_books_list_display(self, page_number) -> Optional[List[ListDisplayBook]]:
+        rows = self.repo.get_book_list_display(page_number)
+        books = list(map(self.make_display_book, rows))
 
-	def get_book(self, book_id) -> Optional[Book]:
-		rows = self.repo.get_book(book_id)
+        return utils.all_or_none(books)
 
-		if len(rows) == 0:
-			return None
+    def get_book(self, book_id) -> Optional[Book]:
+        rows = self.repo.get_book(book_id)
 
-		return self.make_book(rows[0])
+        if len(rows) == 0:
+            return None
 
-	def get_user_checked_books(self, email) -> Optional[List[ListDisplayBook]]:
-		rows = self.repo.get_users_checked_books(email)
+        return self.make_book(rows[0])
 
-		if len(rows) == 0:
-			return None
+    def get_user_checked_books(self, email) -> Optional[List[ListDisplayBook]]:
+        rows = self.repo.get_users_checked_books(email)
 
-		books = list(map(self.make_display_book, rows))
+        if len(rows) == 0:
+            return None
 
-		return utils.all_or_none(books)
+        books = list(map(self.make_display_book, rows))
 
-	def get_book_loaners(self, book_id) -> Optional[List[User]]:
-		rows = self.repo.get_book_loaners(book_id)
+        return utils.all_or_none(books)
 
-		if len(rows) == 0:
-			return None
+    def get_book_loaners(self, book_id) -> Optional[List[User]]:
+        rows = self.repo.get_book_loaners(book_id)
 
-		users = list(map(self.make_user, rows))
+        if len(rows) == 0:
+            return None
 
-		return utils.all_or_none(users)
+        users = list(map(self.make_user, rows))
 
-	def get_available_count(self, book_id) -> int:
-		total_count = self.repo.get_total_copy_count(book_id)
-		checked_count = self.repo.get_checked_copy_count(book_id)
+        return utils.all_or_none(users)
 
-		return total_count - checked_count
+    def get_available_count(self, book_id) -> int:
+        total_count = self.repo.get_total_copy_count(book_id)
+        checked_count = self.repo.get_checked_copy_count(book_id)
 
-	def book_available_for_checkout(self, book_id) -> bool:
-		return self.get_available_count(book_id) > 0
+        return total_count - checked_count
 
-	def email_belongs_to_customer(self, email) -> bool:
-		rows = self.repo.get_num_accounts(email)
-		return rows > 0
+    def book_available_for_checkout(self, book_id) -> bool:
+        return self.get_available_count(book_id) > 0
 
-	def make_user(self, r: Row) -> Optional[User]:
-		try:
-			return User(
-				r.customer_id,
-				r.email,
-				r.name,
-				r.card_inactive
-			)
-		except:
-			print("make_display_user was not given a row it needed")
-			return None
+    def email_belongs_to_customer(self, email) -> bool:
+        rows = self.repo.get_num_accounts(email)
+        return rows > 0
 
-	def make_book(self, r: Row) -> Optional[Book]:
-		try: 
-			book_id = r.book_id
-			available = self.book_available_for_checkout(book_id)
-			available_count = self.get_available_count(book_id)
-			total_count = self.repo.get_total_copy_count(book_id)
+    def make_user(self, r: Row) -> Optional[User]:
+        try:
+            return User(r.customer_id, r.email, r.name, r.card_inactive)
+        except:
+            print("make_display_user was not given a row it needed")
+            return None
 
-			return Book(
-				r.book_id,
-				r.isbn,
-				r.cover_img_url,
-				r.author, 
-				r.title,
-				r.genre,
-				available,
-				available_count,
-				total_count
-			)
-		except:
-			print("make_book was not given a row it needed")
-			return None
+    def make_book(self, r: Row) -> Optional[Book]:
+        try:
+            book_id = r.book_id
+            available = self.book_available_for_checkout(book_id)
+            available_count = self.get_available_count(book_id)
+            total_count = self.repo.get_total_copy_count(book_id)
 
-	def make_display_book(self, r: Row) -> Optional[ListDisplayBook]:
-		try:
-			book_id = r.book_id
-			available = self.book_available_for_checkout(book_id)
-			available_count = self.get_available_count(book_id)
+            return Book(
+                r.book_id,
+                r.isbn,
+                r.cover_img_url,
+                r.author,
+                r.title,
+                r.genre,
+                available,
+                available_count,
+                total_count,
+            )
+        except:
+            print("make_book was not given a row it needed")
+            return None
 
-			return ListDisplayBook(
-				book_id,
-				r.cover_img_url,
-				r.title,
-				r.genre,
-				available,
-				available_count
-			)
-		except:
-			print("make_display_book was not given a row it needed")
-			return None
+    def make_display_book(self, r: Row) -> Optional[ListDisplayBook]:
+        try:
+            book_id = r.book_id
+            available = self.book_available_for_checkout(book_id)
+            available_count = self.get_available_count(book_id)
 
-	def make_checkout(self, r: Row) -> Optional[Checkout]:
-		try:
-			return Checkout(
-				r.checkout_date,
-				r.due_date,
-				r.book_id,
-				r.condition,
-				r.overdue,
-				r.checkout_id,
-				r.book_copy_id
-			)
-		except:
-			print("make_checkout was not given a row it needed")
-			return None
+            return ListDisplayBook(
+                book_id, r.cover_img_url, r.title, r.genre, available, available_count
+            )
+        except:
+            print("make_display_book was not given a row it needed")
+            return None
 
-	def dispose(self):
-		self.repo.dispose()
+    def make_checkout(self, r: Row) -> Optional[Checkout]:
+        try:
+            return Checkout(
+                r.checkout_date,
+                r.due_date,
+                r.book_id,
+                r.condition,
+                r.overdue,
+                r.checkout_id,
+                r.book_copy_id,
+            )
+        except:
+            print("make_checkout was not given a row it needed")
+            return None
+
+    def dispose(self):
+        self.repo.dispose()
--- utils.py
+++ utils.py
@@ -2,36 +2,42 @@ from typing import Optional, List, Any
 from pathlib import Path
 from flask import render_template
 import pyodbc
-import dotenv 
-import os 
+import dotenv
+import os
 
 PAGE_SIZE = 32
+
 
 def get_env_or_exit(env_name: str):
     val = os.getenv(env_name)
 
-    if (val is None):
+    if val is None:
         print("Could not get env variable: " + env_name)
         print("See readme")
         exit(1)
 
     return val
 
+
 def create_database_connection():
     conn_str = get_env_or_exit("SQL_CONNECTION_STRING")
 
     return pyodbc.connect(conn_str)
 
+
 def check_dotenv():
-	if not dotenv.load_dotenv():
-		print("Environment file not found, see readme")
-		exit(1)
+    if not dotenv.load_dotenv():
+        print("Environment file not found, see readme")
+        exit(1)
+
 
 def render_success_failure(message):
-	return render_template('success_failure.html', message=message)
+    return render_template("success_failure.html", message=message)
 
-def all_or_none(l: List[Any]) -> Optional[List[Any]]: 
-    return l if all(l) else None # return none if any element is none
+
+def all_or_none(l: List[Any]) -> Optional[List[Any]]:
+    return l if all(l) else None  # return none if any element is none
+
 
 def get_analytics_query_names():
     IGNORE = ["analytics_input"]
@@ -44,12 +50,15 @@ def get_analytics_query_names():
     if not templates_dir.is_dir():
         return None
 
-    query_names = [x.stem for x in templates_dir.iterdir() if x.is_file() and x.stem not in IGNORE]
+    query_names = [
+        x.stem for x in templates_dir.iterdir() if x.is_file() and x.stem not in IGNORE
+    ]
     return query_names
+
 
 # snake_case -> Snake case
 def snake_case_to_proper(s: str):
-    s = s.replace('_', ' ')
+    s = s.replace("_", " ")
     s = s[0].capitalize() + s[1:]
 
     return s
